a1c9f22f8e558277efb451ee3d9d7b7a051e3e15
lalit raghav <lalit@zero1.io>
2023-02-25 08:40:58 +0530

    first commit

---
+++ resources/js/app.js
@@ -0,0 +1,7 @@
+import './bootstrap';
+
+import Alpine from 'alpinejs';
+
+window.Alpine = Alpine;
+require('@fortawesome/fontawesome-free/js/all.min.js');
+Alpine.start();
---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,27 @@
+<div class="d-flex flex-column flex-shrink-0 p-3 bg-light" style="width: 280px;">
+    <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
+      <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
+      <span class="fs-4">Sidebar</span>
+    </a>
+    <hr>
+    <ul class="nav nav-pills flex-column mb-auto">
+      <li class="nav-item">
+        <a href="/dashboard" class="nav-link active">
+            <i class="fa fa-home" aria-hidden="true"></i>
+            Dashboard
+        </a>
+      </li>
+      <li>
+        <a href="/new-stocks" class="nav-link link-dark">
+          <i class="fa fa-cubes" aria-hidden="true"></i>
+          Blogs
+        </a>
+      </li>
+      <li>
+        <a href="#" class="nav-link link-dark">
+        <i class="fa fa-users" aria-hidden="true"></i>
+          Users
+        </a>
+      </li>
+    </ul>
+  </div>
